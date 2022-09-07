3ebd806543783d810883a4d1b6b279642f4138e3
Jinyoung2000 <sjy021621@gmail.com>
2022-09-07 23:14:51 +0900

    feat: User Entitiy 추가 및 service, controller 구성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import User from './user/user.entity';
 
 @Module({
   imports: [
@@ -13,11 +15,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'root',
       password: '1234',
       database: 'memo',
-      entities: [],
-
+      entities: [User],
       // MEMO: production에서는 사용하면 안됨
       synchronize: true,
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
