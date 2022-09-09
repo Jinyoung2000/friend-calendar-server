fa3cdfc6f33dd1d83be3ad7dd6d1b1bb21b994e3
Jinyoung2000 <sjy021621@gmail.com>
2022-09-09 21:02:52 +0900

    feat: add nest configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,21 +4,27 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import User from './user/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigurationModule } from './config/configuration';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigurationModule],
+      inject: [ConfigService],
       // FIXME: env 사용
-      port: 3306,
-      username: 'root',
-      password: '1234',
-      database: 'memo',
-      entities: [User],
-      // MEMO: production에서는 사용하면 안됨
-      synchronize: true,
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        port: 3306,
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: 'memo',
+        entities: [User],
+        // MEMO: production에서는 사용하면 안됨
+        synchronize: true,
+      }),
     }),
+    ConfigModule,
     UserModule,
   ],
   controllers: [AppController],
